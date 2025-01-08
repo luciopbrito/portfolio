8c3dbce48586fed799dc5885a2f7fc61089c6f42
Lucio Brito <luciopereirabritoo@gmail.com>
2025-01-08 19:33:40 -0300

    add github action for deployment on vps

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: "/portfolio",
+	base: "/",
 	resolve: {
 		alias: {
 			"@imgs": `${__dirname}/src/assets/imgs/`,
